b24ef935e3f9f827d5aae2f45cd330dc295826d6
UB2002 <udaybhaskar0532@gmail.com>
2024-07-06 21:15:17 +0530

    logo add

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,35 @@
-import React  from 'react';
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+//import { useTheme } from '@mui/material/styles';
+import logo from '../assests/logo.png';  // Adjust the path to your logo image
 
 export default function NavBar() {
+  //const theme = useTheme();  // To access theme properties
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static"  sx={{ backgroundColor: 'white', color: 'black' }}>
+      <AppBar position="static" sx={{ backgroundColor: 'white', color: 'black' }}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+          <Box
+            component="img"
+            src={logo}
+            alt="Logo"
+            sx={{ height: 40, mr: 2 }}  // Adjust height as needed
+          />
+           
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+          <Button variant="contained" color="primary" type="courses">
+            Courses
+          </Button>
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit"> Refer & Earn</Button>
+          <Button color="inherit"> Resources</Button>
+          <Button color="inherit"> About Us</Button>
+          <Button variant="contained" color="primary">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
